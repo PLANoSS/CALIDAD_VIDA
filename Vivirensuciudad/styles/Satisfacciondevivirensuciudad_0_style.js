8b415db42433487e5b5d1bcc42178b9d06adb257
PLANoSS <freyesbueno@gmail.com>
2019-03-13 15:05:39 -0500

    Satisfacción de Vivir en su ciudad

--- Vivirensuciudad/styles/Satisfacciondevivirensuciudad_0_style.js
+++ Vivirensuciudad/styles/Satisfacciondevivirensuciudad_0_style.js
@@ -6,7 +6,7 @@ var style_Satisfacciondevivirensuciudad_0 = function(feature, resolution){
         feature: feature,
         variables: {}
     };
-    var value = feature.get("q2wHide_Muy de acu");
+    var value = feature.get("Muy de acuerdo (%)");
     var labelText = "";
     size = 0;
     var labelFont = "9.75px \'MS Shell Dlg 2\', sans-serif";
