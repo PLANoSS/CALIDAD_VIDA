bcb1af70ee2171b50b33fd82e8492eae27426e69
PLANoSS <freyesbueno@gmail.com>
2019-03-13 14:59:40 -0500

    Limpieza

--- Limpieza/styles/Limpiezadesuciudad_0_style.js
+++ Limpieza/styles/Limpiezadesuciudad_0_style.js
@@ -6,7 +6,7 @@ var style_Limpiezadesuciudad_0 = function(feature, resolution){
         feature: feature,
         variables: {}
     };
-    var value = feature.get("q2wHide_Algo satis");
+    var value = feature.get("Algo satisfecho (%)");
     var labelText = "";
     size = 0;
     var labelFont = "9.75px \'MS Shell Dlg 2\', sans-serif";
